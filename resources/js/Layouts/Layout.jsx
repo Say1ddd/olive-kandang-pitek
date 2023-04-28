ae839d9e8a446edf4feb88eac5a6c14649791c15
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-04-29 00:26:36 +0700

    Added: react icons for accessibility

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -4,6 +4,7 @@ import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import Dropdown from '@/Components/Dropdown';
 import Footer from '@/Components/Footer';
+import { HiClipboard, HiHome, HiOutlineLogin, HiOutlineLogout, HiOutlineMenu, HiQuestionMarkCircle } from 'react-icons/hi'
 
 export default function Layout({ user, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
@@ -14,8 +15,9 @@ export default function Layout({ user, children }) {
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
                         <div className="flex">
-                            <div className="shrink-0 flex items-center">
+                            <div className="shrink-0 flex items-center my-5">
                                 <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-100" />
+                                <div className='font-bold text-2xl bg-gradient-to-r from-white text-transparent bg-clip-text to-sky-400 ml-2'>KANGTEK</div>
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
@@ -23,18 +25,21 @@ export default function Layout({ user, children }) {
                                     href={route('dashboard')}
                                     active={route().current('dashboard')}
                                 >
+                                    <HiHome className='h-4 mb-1 mr-1 w-4'/>
                                     Dashboard
                                 </NavLink>
                                 <NavLink
                                     href={route('sensor')}
                                     active={route().current('sensor')}
                                 >
+                                    <HiClipboard className='h-4 mb-1 mr-1 w-4'/>
                                     Manage (login required)
                                 </NavLink>
                                 <NavLink
                                     href={route('about')}
                                     active={route().current('about')}
                                 >
+                                    <HiQuestionMarkCircle className='h-4 mb-1 mr-1 w-4'/>
                                     About
                                 </NavLink>
                             </div>
@@ -81,6 +86,7 @@ export default function Layout({ user, children }) {
                             {!user && (
                                 <div className="ms-3">
                                     <NavLink href={route('login')}>
+                                    <HiOutlineLogin className='h-4 w-4 mr-2' />
                                         Login
                                     </NavLink>
                                 </div>
@@ -92,7 +98,7 @@ export default function Layout({ user, children }) {
                                 onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
                                 className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out"
                             >
-
+                                <HiOutlineMenu className="h-6 w-6" />
                             </button>
                         </div>
                     </div>
@@ -101,9 +107,15 @@ export default function Layout({ user, children }) {
                 <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
                     <div className="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
+                        <HiHome className='h-4 mr-1 w-4 mt-0.5'/>
                             Dashboard
                         </ResponsiveNavLink>
+                        <ResponsiveNavLink href={route('sensor')} active={route().current('sensor')}>
+                        <HiClipboard className='h-4 mr-1 w-4 mt-0.5'/>
+                            Manage (require login)
+                        </ResponsiveNavLink>
                         <ResponsiveNavLink href={route('about')} active={route().current('about')}>
+                        <HiQuestionMarkCircle className='h-4 mr-1 w-4 mt-0.5'/>
                             About
                         </ResponsiveNavLink>
                     </div>
@@ -118,6 +130,7 @@ export default function Layout({ user, children }) {
                             <div className="mt-3 space-y-1">
                             <ResponsiveNavLink href={route('profile.edit')}>Profile</ResponsiveNavLink>
                             <ResponsiveNavLink method="post" href={route('logout')} as="button">
+                            <HiOutlineLogout className='h-4 w-4 mr-2' />
                                 Log Out
                             </ResponsiveNavLink>
                         </div>
