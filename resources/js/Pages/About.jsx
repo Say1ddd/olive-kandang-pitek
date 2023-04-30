861048bd95bc7b4a97a5b671a656b6e4d98b717d
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-05-01 00:28:40 +0700

    Modify: adjustment for responsive design

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -5,10 +5,10 @@ import ListComponent from "@/Components/ListComponent";
 
 const About = ({ auth }) => {
   const members = [
-    { name: 'Kayla Aprilia Islamy Putri', as: 'Product Designer'},
-    { name: 'Marsha Nafisa Putri', as: 'Product Engineer'},
-    { name: 'Muhammad Abdul Aziz', as: 'Database Engineer'},
-    { name: 'Muhammad Rasyid', as: 'Product Engineer'},
+    { name: 'Kayla Aprilia Islamy Putri', as: 'Designer'},
+    { name: 'Marsha Nafisa Putri', as: 'Producer'},
+    { name: 'Muhammad Abdul Aziz', as: 'DBA'},
+    { name: 'Muhammad Rasyid', as: 'Producer'},
     { name: 'Naufalikho Abyan', as: 'Programmer'},
     { name: 'Sajid Muhammad Ikhlas', as: 'Developer'},
     { name: 'Zulham Daninoor', as: 'DevOps'},
@@ -17,16 +17,15 @@ const About = ({ auth }) => {
   return (
     <Layout
     user={auth.user}
-    header={<h2 className="font-semibold text-xl text-gray-800 dark:text-white leading-tight">About</h2>}
 >
     <Head title="About" />
 
       <NavLink href={route('dashboard')} active={route().current('dashboard')} />
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="bg-black/20 dark:text-white hover:text-white duration-300 overflow-hidden shadow-sm sm:rounded-lg">
-          <div className="p-6">
-          <div className="text-3xl font-bold mb-4">Welcome to KANGTEK</div>
-          <div className="text-lg mb-4">KANGTEK stands for KAndaNG TEKnologi, a platform designed to monitor and control IoT devices. This platform integrates advanced sensors, data analytics, and machine learning algorithms to provide real-time insights and automate various processes, enabling businesses to optimize their operations, improve efficiency, and enhance decision-making capabilities, ultimately leading to increased productivity and competitiveness in today's rapidly evolving technological landscape.</div>
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="bg-black/20 dark:text-white hover:text-white duration-300 overflow-hidden shadow-sm p-4 sm:rounded-lg">
+          <div className="p-2">
+          <div className="text-xl md:text-2xl lg:text-3xl font-bold mb-4 text-center">Welcome to <span className=" bg-gradient-to-r from-white to-sky-300 bg-clip-text text-transparent">KANGTEK</span></div>
+          <div className="text-sm text-justify md:text-md lg:text-lg mb-4">KANGTEK stands for KAndaNG TEKnologi, a platform designed to monitor and control IoT devices. This platform integrates advanced sensors, data analytics, and machine learning algorithms to provide real-time insights and automate various processes, enabling businesses to optimize their operations, improve efficiency, and enhance decision-making capabilities, ultimately leading to increased productivity and competitiveness in today's rapidly evolving technological landscape.</div>
           <ListComponent members={members} />
           </div>
         </div>
