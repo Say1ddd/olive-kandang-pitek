ac746ba5424fff85f6d3cc85e51e23c7e654db7a
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-05-02 00:29:58 +0700

    Fix: inconsistent structure

--- resources/js/Components/ListComponent.jsx
+++ resources/js/Components/ListComponent.jsx
@@ -1,19 +1,19 @@
 const ListComponent = ({ members }) => {
-  return (
-      <div className="bg-black/25 p-5 rounded">
-      <div className="font-bold">Group Members</div>
-          <ol>
-              {members.map((member, index) => (
-                  <li key={index} className="flex items-center my-2 rounded p-2 mx-4 bg-black/25 border-l-8 border-transparent hover:border-white duration-300 pl-2">
-                      <div className="text-lg">{member.name}</div>
-                      <span className={`border p-1 rounded-full text-xs ml-2 border-sky-400 text-sky-200 ${member.as ? member.as : 'border-red-500 text-red-500'}`}>
-                          {member.as ? member.as : "Beban"}
-                      </span>
-                  </li>
-              ))}
-          </ol>
-      </div>
-  );
+    return (
+        <div className="bg-black/25 p-5 rounded">
+        <div className="font-bold text-center">Group Members</div>
+            <ol>
+                {members.map((member, index) => (
+                    <li key={index} className="flex items-center my-2 rounded p-2 bg-black/25 justify-between md:justify-center md:mx-4 border-x-4 border-transparent hover:border-sky-300 hover:bg duration-300 pl-2">
+                        <div className="text-sm md:text-md lg:text-lg">{member.name}</div>
+                        <span className={`border p-1 rounded-full text-xs md:text-sm lg:text-md ml-1 lg:ml-3 border-sky-400 text-sky-200 ${member.as ? member.as : 'border-red-500 text-red-500'}`}>
+                            {member.as ? member.as : "Beban"}
+                        </span>
+                    </li>
+                ))}
+            </ol>
+        </div>
+    );
 };
 
 export default ListComponent;
--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -2,9 +2,10 @@ import ApplicationLogo from '@/Components/svg/ApplicationLogo';
 
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <ApplicationLogo className="w-40 h-40 fill-current text-gray-100" />
+        <div className="min-h-screen flex flex-col justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+            <div className='flex justify-center items-center'>
+                <ApplicationLogo className="w-12 h-12 fill-current text-gray-100" />
+                <div className='text-2xl font-bold bg-gradient-to-r from-white to-sky-300 text-transparent bg-clip-text'>KANGTEK</div>
             </div>
 
             <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
